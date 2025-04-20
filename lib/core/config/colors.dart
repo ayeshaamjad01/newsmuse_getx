7a71fefd8e0f15255b87cf007afa174e17858c87
ayeshaamjad01 <ashamjad007@gmail.com>
2025-04-20 14:05:27 +0500

    creating themes light & dark)

--- lib/core/config/colors.dart
+++ lib/core/config/colors.dart
@@ -5,4 +5,20 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primary = Color.fromARGB(255, 85, 107, 255);
   static const Color screenBackground = Color.fromARGB(255, 234, 234, 234);
+  static const Color paymentModeColor = Color.fromARGB(255, 85, 107, 255);
+  static const Color paymentResionColor = Color.fromARGB(255, 234, 234, 234);
+
+  //light theme colors
+  static const Color lightBgColor = Color.fromARGB(255, 85, 107, 255);
+  static const Color lightFontColor = Color.fromARGB(255, 234, 234, 234);
+  static const Color lightDivColor = Color.fromARGB(255, 85, 107, 255);
+  static const Color lightPrimaryColor = Color.fromARGB(255, 234, 234, 234);
+  static const Color lightLabelColor = Color.fromARGB(255, 85, 107, 255);
+
+  //dark theme colors
+  static const Color darkBgColor = Color.fromARGB(255, 85, 107, 255);
+  static const Color darkFontColor = Color.fromARGB(255, 234, 234, 234);
+  static const Color darkDivColor = Color.fromARGB(255, 85, 107, 255);
+  static const Color darkPrimaryColor = Color.fromARGB(255, 234, 234, 234);
+  static const Color darkLabelColor = Color.fromARGB(255, 85, 107, 255);
 }
--- lib/core/config/theme.dart
+++ lib/core/config/theme.dart
@@ -1,12 +1,42 @@
 import 'package:newsmuse/core/config/colors.dart';
 import 'package:flutter/material.dart';
 
-class AppTheme {
-  static ThemeData countCalTheme = ThemeData(
-    scaffoldBackgroundColor: AppColors.screenBackground,
-    fontFamily: 'Rubik',
-    // ✅ Set the default font for the whole app
-    // primarySwatch: Colors.green,
-    primaryColor: Color.fromARGB(255, 85, 107, 255),
-  );
-}
+// class AppTheme {
+//   static ThemeData countCalTheme = ThemeData(
+//     scaffoldBackgroundColor: AppColors.screenBackground,
+//     fontFamily: 'Rubik',
+//     // ✅ Set the default font for the whole app
+//     // primarySwatch: Colors.green,
+//     primaryColor: Color.fromARGB(255, 85, 107, 255),
+//   );
+// }
+
+var lightTheme = ThemeData(
+  useMaterial3: true,
+  inputDecorationTheme: const InputDecorationTheme(
+    fillColor: AppColors.lightBgColor,
+    filled: true,
+    enabledBorder: InputBorder.none,
+    prefixIconColor: AppColors.lightLabelColor,
+    labelStyle: TextStyle(
+      fontFamily: "Poppins",
+      fontSize: 15,
+      color: AppColors.lightFontColor,
+      fontWeight: FontWeight.w500,
+    ),
+    hintStyle: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 15,
+        color: AppColors.lightFontColor,
+        fontWeight: FontWeight.w500),
+  ),
+  colorScheme: const ColorScheme.light(
+    brightness: Brightness.light,
+    surface: AppColors.lightBgColor,
+    onSurface: AppColors.lightFontColor,
+    primaryContainer: AppColors.lightDivColor,
+    onPrimaryContainer: AppColors.lightFontColor,
+    secondaryContainer: AppColors.lightLabelColor,
+    primary: AppColors.lightPrimaryColor,
+  ),
+);
