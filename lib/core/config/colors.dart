44446302202475e5ca59480a5478239cc5ac5e09
ayeshaamjad01 <ashamjad007@gmail.com>
2025-04-21 14:47:12 +0500

    colors added to project

--- lib/core/config/colors.dart
+++ lib/core/config/colors.dart
@@ -9,16 +9,16 @@ class AppColors {
   static const Color paymentResionColor = Color.fromARGB(255, 234, 234, 234);
 
   //light theme colors
-  static const Color lightBgColor = Color.fromARGB(255, 85, 107, 255);
-  static const Color lightFontColor = Color.fromARGB(255, 234, 234, 234);
-  static const Color lightDivColor = Color.fromARGB(255, 85, 107, 255);
-  static const Color lightPrimaryColor = Color.fromARGB(255, 234, 234, 234);
-  static const Color lightLabelColor = Color.fromARGB(255, 85, 107, 255);
+  static const Color lightBgColor = Color(0xfff8f9fd);
+  static const Color lightFontColor = Color(0xff1f1f1f);
+  static const Color lightDivColor = Color(0xffe2e6f3);
+  static const Color lightPrimaryColor = Color(0xff556bff);
+  static const Color lightLabelColor = Color(0xff5f6c85);
 
   //dark theme colors
-  static const Color darkBgColor = Color.fromARGB(255, 85, 107, 255);
-  static const Color darkFontColor = Color.fromARGB(255, 234, 234, 234);
-  static const Color darkDivColor = Color.fromARGB(255, 85, 107, 255);
-  static const Color darkPrimaryColor = Color.fromARGB(255, 234, 234, 234);
-  static const Color darkLabelColor = Color.fromARGB(255, 85, 107, 255);
+  static const Color darkPrimaryColor = Color(0xff556bff);
+  static const Color darkBgColor = Color(0xff10131c);
+  static const Color darkFontColor = Color(0xfff1f2f6);
+  static const Color darkLabelColor = Color(0xff9aa5c0);
+  static const Color darkDivColor = Color(0xff1e2232);
 }
--- lib/core/config/theme.dart
+++ lib/core/config/theme.dart
@@ -12,31 +12,158 @@ import 'package:flutter/material.dart';
 // }
 
 var lightTheme = ThemeData(
-  useMaterial3: true,
-  inputDecorationTheme: const InputDecorationTheme(
-    fillColor: AppColors.lightBgColor,
-    filled: true,
-    enabledBorder: InputBorder.none,
-    prefixIconColor: AppColors.lightLabelColor,
-    labelStyle: TextStyle(
-      fontFamily: "Poppins",
-      fontSize: 15,
-      color: AppColors.lightFontColor,
-      fontWeight: FontWeight.w500,
-    ),
-    hintStyle: TextStyle(
+    useMaterial3: true,
+    inputDecorationTheme: const InputDecorationTheme(
+      fillColor: AppColors.lightBgColor,
+      filled: true,
+      enabledBorder: InputBorder.none,
+      prefixIconColor: AppColors.lightLabelColor,
+      labelStyle: TextStyle(
         fontFamily: "Poppins",
         fontSize: 15,
         color: AppColors.lightFontColor,
-        fontWeight: FontWeight.w500),
-  ),
-  colorScheme: const ColorScheme.light(
-    brightness: Brightness.light,
-    surface: AppColors.lightBgColor,
-    onSurface: AppColors.lightFontColor,
-    primaryContainer: AppColors.lightDivColor,
-    onPrimaryContainer: AppColors.lightFontColor,
-    secondaryContainer: AppColors.lightLabelColor,
-    primary: AppColors.lightPrimaryColor,
-  ),
-);
+        fontWeight: FontWeight.w500,
+      ),
+      hintStyle: TextStyle(
+          fontFamily: "Poppins",
+          fontSize: 15,
+          color: AppColors.lightFontColor,
+          fontWeight: FontWeight.w500),
+    ),
+    colorScheme: const ColorScheme.light(
+      brightness: Brightness.light,
+      surface: AppColors.lightBgColor,
+      onSurface: AppColors.lightFontColor,
+      primaryContainer: AppColors.lightDivColor,
+      onPrimaryContainer: AppColors.lightFontColor,
+      secondaryContainer: AppColors.lightLabelColor,
+      primary: AppColors.lightPrimaryColor,
+    ),
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 24,
+        color: AppColors.lightFontColor,
+        fontWeight: FontWeight.w700,
+      ),
+      headlineMedium: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 20,
+        color: AppColors.lightFontColor,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineSmall: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 15,
+        color: AppColors.lightFontColor,
+        fontWeight: FontWeight.w600,
+      ),
+      bodyLarge: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 16,
+        color: AppColors.lightFontColor,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyMedium: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 15,
+        color: AppColors.lightFontColor,
+        fontWeight: FontWeight.w400,
+      ),
+      bodySmall: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 13,
+        color: AppColors.lightFontColor,
+        fontWeight: FontWeight.w500,
+      ),
+      labelSmall: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 13,
+        color: AppColors.lightLabelColor,
+        fontWeight: FontWeight.w300,
+      ),
+    ));
+
+var darkTheme = ThemeData(
+    useMaterial3: true,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.darkDivColor,
+      foregroundColor: AppColors.darkFontColor,
+      elevation: 0,
+      iconTheme: IconThemeData(color: AppColors.darkFontColor),
+      titleTextStyle: TextStyle(
+          fontFamily: "Poppins",
+          fontSize: 20,
+          color: AppColors.darkFontColor,
+          fontWeight: FontWeight.w600),
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      fillColor: AppColors.darkBgColor,
+      filled: true,
+      enabledBorder: InputBorder.none,
+      prefixIconColor: AppColors.darkLabelColor,
+      labelStyle: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 15,
+        color: AppColors.darkFontColor,
+        fontWeight: FontWeight.w500,
+      ),
+      hintStyle: TextStyle(
+          fontFamily: "Poppins",
+          fontSize: 15,
+          color: AppColors.darkFontColor,
+          fontWeight: FontWeight.w500),
+    ),
+    colorScheme: const ColorScheme.dark(
+      brightness: Brightness.dark,
+      surface: AppColors.darkBgColor,
+      onSurface: AppColors.darkFontColor,
+      primaryContainer: AppColors.darkDivColor,
+      onPrimaryContainer: AppColors.darkFontColor,
+      secondaryContainer: AppColors.darkLabelColor,
+      primary: AppColors.darkPrimaryColor,
+    ),
+    textTheme: const TextTheme(
+      headlineLarge: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 24,
+        color: AppColors.darkFontColor,
+        fontWeight: FontWeight.w700,
+      ),
+      headlineMedium: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 20,
+        color: AppColors.darkFontColor,
+        fontWeight: FontWeight.w600,
+      ),
+      headlineSmall: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 15,
+        color: AppColors.darkFontColor,
+        fontWeight: FontWeight.w600,
+      ),
+      bodyLarge: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 16,
+        color: AppColors.darkFontColor,
+        fontWeight: FontWeight.w500,
+      ),
+      bodyMedium: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 15,
+        color: AppColors.darkFontColor,
+        fontWeight: FontWeight.w400,
+      ),
+      bodySmall: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 13,
+        color: AppColors.darkFontColor,
+        fontWeight: FontWeight.w500,
+      ),
+      labelSmall: TextStyle(
+        fontFamily: "Poppins",
+        fontSize: 13,
+        color: AppColors.darkLabelColor,
+        fontWeight: FontWeight.w300,
+      ),
+    ));
